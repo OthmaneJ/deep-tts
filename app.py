fbd4a10e1c1ce53e955b021d95d8abd1d1caa43b
OthmaneJ <jebbariothmane2@gmail.com>
2021-04-30 14:21:30 +0200

    audio with io buffer

--- app.py
+++ app.py
@@ -15,48 +15,30 @@ import math
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-# import plotly.express as px
-# from IPython.display import Audio
-# from IPython.utils import io
-# from synthesizer.inference import Synthesizer
-# from encoder import inference as encoder
-# from vocoder import inference as vocoder
+
 from pathlib import Path
-# import numpy as np
 import scipy
 import soundfile as sf
 import json
-# import time
 
-# from librosa.output import write_wav
+from io import BytesIO
+import base64
 
-speedyspeech_checkpoint = 'checkpoints/speedyspeech.pth'
-melgan_checkpoint = 'checkpoints/melgan.pth'
+from scipy.io.wavfile import write
+import numpy as np
+
+
+speedyspeech_checkpoint = './checkpoints/speedyspeech.pth'
+melgan_checkpoint = './checkpoints/melgan.pth'
 device = 'cpu'
 audio_folder = 'assets'
 input_text = "Get a complete visualization of your app in a team-based continuous delivery environment"
-
-
-# with open('latest_embeddings.json') as f:
-#   new_embeddings = json.load(f)
-  
-# celebrities = [el['name'] for el in new_embeddings]
-
-# encoder_weights = Path("./encoder/saved_models/pretrained.pt")
-# vocoder_weights = Path("./vocoder/saved_models/pretrained.pt")
-# syn_dir = Path("./synthesizer/saved_models/pretrained/pretrained.pt")
-# encoder.load_model(encoder_weights)
-# synthesizer = Synthesizer(syn_dir)
-# vocoder.load_model(vocoder_weights)
-
 
 external_stylesheets = [
     "https://use.fontawesome.com/releases/v5.0.7/css/all.css",
     'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
     'https://fonts.googleapis.com/css?family=Roboto&display=swap'
 ]
-
-# app = dash.Dash(__name__)
 
 app = dash.Dash(
     __name__, 
@@ -101,14 +83,66 @@ app.layout = html.Div(
     ]
     ,style={'textAlign': 'center','marginRight':'100px','marginLeft':'100px','marginTop':'50px','marginBottom':'50px'})
 
-# # Set picture of celebrity
-# @app.callback(
-#     dash.dependencies.Output('celebrity_img','src'),
-#     [dash.dependencies.Input('celebrity-dropdown','value')]
-# )
 
-# def display_image(celebrity):
-#   return new_embeddings[celebrity]['img']
+class SpeedySpeechInference:
+    def __init__(self, speedyspeech_checkpoint, melgan_checkpoint, device):
+        self.device = device
+        self.speedyspeech = self._setup_speedyspeech(speedyspeech_checkpoint)
+        self.melgan = self._setup_melgan(melgan_checkpoint)
+        self.txt_processor = TextProcessor(HPText.graphemes, phonemize=HPText.use_phonemes)
+        self.bit_depth = 16
+        self.sample_rate = 22050
+
+    def _setup_speedyspeech(self, checkpoint):
+        speedyspeech = SpeedySpeech(
+            device=self.device
+        ).load(checkpoint, map_location=self.device)
+        speedyspeech.eval()
+        return speedyspeech
+
+    def _setup_melgan(self, checkpoint):
+        checkpoint = torch.load(checkpoint, map_location=self.device)
+        hp = HParam("./speech/melgan/config/default.yaml")
+        melgan = Generator(hp.audio.n_mel_channels).to(self.device)
+        melgan.load_state_dict(checkpoint["model_g"])
+        melgan.eval(inference=False)
+        return melgan
+
+    def synthesize(self, input_text):
+        print(input_text)
+        text = [input_text.strip()]
+        phonemes, plen = self.txt_processor(text)
+
+        # append more zeros - avoid cutoff at the end of the largest sequence
+        phonemes = torch.cat((phonemes, torch.zeros(len(phonemes), 5).long() ), dim=-1)
+        phonemes = phonemes.to(self.device)
+
+        # generate spectrograms
+        with torch.no_grad():
+            spec, durations = self.speedyspeech((phonemes, plen))
+
+        # invert to log(mel-spectrogram)
+        spec = self.speedyspeech.collate.norm.inverse(spec)
+
+        # mask with pad value expected by MelGan
+        msk = mask(spec.shape, durations.sum(dim=-1).long(), dim=1).to(self.device)
+        spec = spec.masked_fill(~msk, -11.5129)
+
+        # Append more pad frames to improve end of the longest sequence
+        spec = torch.cat((
+            spec.transpose(2,1),
+            -11.5129 * torch.ones(len(spec), HPStft.n_mel, 5).to(self.device)
+        ), dim=-1)
+
+        # generate audio
+        with torch.no_grad():
+            audio = self.melgan(spec).squeeze(1)
+            audio = audio.detach().cpu().numpy()[0]
+
+        # denormalize
+        x = 2 ** self.bit_depth - 1
+        audio = np.int16(audio * x)
+        return audio
 
 
 # Transcribe audio
@@ -131,77 +165,25 @@ def display_warning(n_clicks,value):
     dash.dependencies.Output("player", "src"),
     [dash.dependencies.Input("submit","n_clicks"),
      ],
-    # [dash.dependencies.State("celebrity-dropdown","value"),
     [dash.dependencies.State("transcription_input", "value")],
 )
 
 def vocalize(n_clicks,value):
     
-    input_text = value
-
-    speedyspeech_checkpoint = 'checkpoints/speedyspeech.pth'
-    melgan_checkpoint = 'checkpoints/melgan.pth'
-    device = 'cpu'
-    audio_folder = 'assets'
-    input_text = "Get a complete visualization of your app in a team-based continuous delivery environment"
-
-    print('Loading model checkpoints')
-    m = SpeedySpeech(
-        device=device
-    ).load(speedyspeech_checkpoint, map_location=device)
-    m.eval()
-
-    checkpoint = torch.load(melgan_checkpoint, map_location=device)
-    hp = HParam("speech/melgan/config/default.yaml")
-    melgan = Generator(hp.audio.n_mel_channels).to(device)
-    melgan.load_state_dict(checkpoint["model_g"])
-    melgan.eval(inference=False)
-
-    with open('./counter.txt','r') as f:
-        counter = int(f.read())
-    print(f'number of files : {counter}')
-    
-    print('Processing text')
-    txt_processor = TextProcessor(HPText.graphemes, phonemize=HPText.use_phonemes)
-    text = [input_text]
-
-    phonemes, plen = txt_processor(text)
-    # append more zeros - avoid cutoff at the end of the largest sequence
-    phonemes = torch.cat((phonemes, torch.zeros(len(phonemes), 5).long() ), dim=-1)
-    phonemes = phonemes.to(device)
-
-    print('Synthesizing')
-    # generate spectrograms
-    with torch.no_grad():
-        spec, durations = m((phonemes, plen))
-
-
-    # invert to log(mel-spectrogram)
-    spec = m.collate.norm.inverse(spec)
-
-    # mask with pad value expected by MelGan
-    msk = mask(spec.shape, durations.sum(dim=-1).long(), dim=1).to(device)
-    spec = spec.masked_fill(~msk, -11.5129)
-
-    # Append more pad frames to improve end of the longest sequence
-    spec = torch.cat((spec.transpose(2,1), -11.5129*torch.ones(len(spec), HPStft.n_mel, 5).to(device)), dim=-1)
-
-    # generate audio
-    with torch.no_grad():
-        audio = melgan(spec).squeeze(1)
-
     print('Saving audio')
-    # TODO: cut audios to proper length
-    audio_folder = '/assets'
-    # for i,a in enumerate(audio.detach().cpu().numpy()):
-        # write_wav(os.path.join(audio_folder,f'{i}.wav'), a, HPStft.sample_rate, norm=False)
-    sf.write(os.path.join(audio_folder,f'{counter}.wav'), audio.detach().cpu().numpy()[0], HPStft.sample_rate)
-    counter +=1
-    # time.sleep(10)
-    with open('./counter.txt','w') as f:
-        f.write(str(counter))
 
-    return f'/assets/{counter-1}.wav'
+    speedyspeech = SpeedySpeechInference(
+        speedyspeech_checkpoint,
+        melgan_checkpoint,
+        device
+    )
+    
+    input_text = value
+    buf = BytesIO()
+    waveform_integers = speedyspeech.synthesize(input_text)
+    write(buf, speedyspeech.sample_rate, waveform_integers)
+
+    return f'data:audio/wav;base64,{base64.b64encode(buf.getvalue()).decode()}'
 
 @app.callback(
     dash.dependencies.Output("rating-message", "value"),
@@ -216,17 +198,5 @@ def print_rating(n_clicks,rating):
 
 
 if __name__ == "__main__":
-
-    # print('Loading model checkpoints')
-    # m = SpeedySpeech(
-    #     device=device
-    # ).load(speedyspeech_checkpoint, map_location=device)
-    # m.eval()
-
-    # checkpoint = torch.load(melgan_checkpoint, map_location=device)
-    # hp = HParam("speech/melgan/config/default.yaml")
-    # melgan = Generator(hp.audio.n_mel_channels).to(device)
-    # melgan.load_state_dict(checkpoint["model_g"])
-    # melgan.eval(inference=False)
 
     app.run_server(debug=False)
